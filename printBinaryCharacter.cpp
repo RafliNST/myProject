6fde7d53f5e879de46a3051d3bc5e55a045b5979
RafliNST <rafli.ran3103@gmail.com>
2023-05-06 08:34:09 +0700

    @RANst: update

--- printBinaryCharacter.cpp
+++ printBinaryCharacter.cpp
@@ -7,28 +7,27 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     char nama[75];
+    do{
+        cout << "Masukkan Kalimat Untuk Ditranslasi: ";
+        cin.getline(nama, 75);
 
-    cout << "Masukkan Nama Untuk Ditranslasi: ";
-    cin.getline(nama, 75);
-
-    for(int i = 0, length = strlen(nama); i < length; i++){
-        char binary[] = "00000000";
-        int binary_len = strlen(binary)-1;
-        int bin = (int)nama[i];
+        for(int i = 0, length = strlen(nama); i < length; i++){
+            char binary[] = "00000000";
+            int binary_len = strlen(binary)-1;
+            int bin = (int)nama[i];
 
 
-        while (bin > 0){
-            if(bin%2 == 0)
-                binary[binary_len] = '0';
-            else
-                binary[binary_len] = '1';
+            while (bin > 0){
+                binary[binary_len] = (bin%2 == 0)? '0' : '1';
+                
+                bin = bin/2;
+                binary_len--;
+            }
             
-            bin = bin/2;
-            binary_len--;
+            cout << setiosflags(ios::left) << setw(5);
+            cout << ends << nama[i] <<" : "<< setw(5) << setiosflags(ios::left);
+            cout << (int)nama[i] << " -> " << setw(2) << binary << endl;
         }
-        
-        cout << setiosflags(ios::left) << setw(5);
-        cout << ends << nama[i] <<" : "<< setw(5) << setiosflags(ios::left) << (int)nama[i] << " -> " << setw(2) << binary << endl;
-    }
+    } while(strcmp( nama, "") != 0);
     return 0;
 }
